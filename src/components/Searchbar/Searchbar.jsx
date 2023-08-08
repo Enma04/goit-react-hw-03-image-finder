a5cab4fe27b701b160a03bcbd2d696ac8b48aad0
Enma04 <118761090+Enma04@users.noreply.github.com>
2023-08-09 00:28:06 +1000

    updated

--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-export default class Searchbar extends Component {
+export default class Searchbar extends React.Component {
     state = {  } 
     render() { 
         return (
