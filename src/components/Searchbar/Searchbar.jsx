b2a6f977a15c3e09488ce7a8cd11fed6a1563b13
Enma04 <118761090+Enma04@users.noreply.github.com>
2023-08-09 00:26:43 +1000

    Creacion de directorios

---
+++ src/components/Searchbar/Searchbar.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default class Searchbar extends Component {
+    state = {  } 
+    render() { 
+        return (
+            <div></div>
+        );
+    }
+}
