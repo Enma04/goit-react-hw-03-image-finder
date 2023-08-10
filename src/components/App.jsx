7f46c17b7ddd44702b7404da743a90e4eedf7844
Enma04 <118761090+Enma04@users.noreply.github.com>
2023-08-11 01:45:41 +1000

    Realizada la peticion correctamente con datos

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,36 +1,33 @@
 import React, { Component } from 'react';
-import fetchPixabay from '../resources/fetchPixabay';
+import { fetchPixabay } from '../resources/fetchPixabay';
 
 export class App extends Component {
 
   constructor(props) {
     super(props);
 
-    this.state = {}
+    this.state = {
+      images: [],
+    }
   }
 
-  componentDidMount() {
-    const myKey = "38579369-eeb8272d66c6603b69812822b";
-    const data = fetch(`https://pixabay.com/api/?q=cat&page=1&key=${myKey}&image_type=photo&orientation=horizontal&per_page=12`)
-      .then(
-        (response) => {
-          if (!response.ok) {
-            throw new Error(response.status);
-          }
-          return response.json();
-        }
-      )
-      //.then( info => console.log("info: ", info) );
-
-    const data2 = fetchPixabay();
-
-    console.log("fetch: ", data2);
+  async componentDidMount() {
+    //const data2 = fetchPixabayAxios().then((resp) => resp.data);
+    const data = await fetchPixabay().then();
+    //const resp = data resp.then();
+    this.setState(() => ({
+      images: [...data.hits],
+    }));
   }
   
   render() {
+    const { images } = this.state;
+    console.log("images: ", images);
+
     return (
       <div>
         React homework template
+        <h2>Imagenes</h2>
       </div>
     );
   }
--- src/resources/fetchPixabay.js
+++ src/resources/fetchPixabay.js
@@ -1,10 +1,17 @@
-export default function fetchPixabay() {
-  const myKey = '38579369-eeb8272d66c6603b69812822b';
-  return fetch(`https://pixabay.com/api/?q=cat&page=1&key=${myKey}&image_type=photo&orientation=horizontal&per_page=12`)
+import axios from 'axios';
+
+const myKey = '38579369-eeb8272d66c6603b69812822b';
+
+export const fetchPixabay = async () => {
+  return await fetch(`https://pixabay.com/api/?q=cat&page=1&key=${myKey}&image_type=photo&orientation=horizontal&per_page=12`)
     .then(response => {
         if (!response.ok) {
             throw new Error(response.status);
         }
         return response.json();
     });
+};
+
+export const fetchPixabayAxios = async () => {
+    return await axios.get(`https://pixabay.com/api/?q=cat&page=1&key=${myKey}&image_type=photo&orientation=horizontal&per_page=12`);
 };
