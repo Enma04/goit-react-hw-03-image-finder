24ac4a7385f528965562dd54feba1a2c29fe6132
Enma04 <118761090+Enma04@users.noreply.github.com>
2023-08-12 01:53:47 +1000

    change params

--- src/resources/fetchPixabay.js
+++ src/resources/fetchPixabay.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const myKey = '38579369-eeb8272d66c6603b69812822b';
 
 export const fetchPixabay = async () => {
-  return await fetch(`https://pixabay.com/api/?q=cat&page=1&key=${myKey}&image_type=photo&orientation=horizontal&per_page=12`)
+  return await fetch(`https://pixabay.com/api/?q=dog&page=3&per_page=12&key=${myKey}&image_type=photo&orientation=horizontal`)
     .then(response => {
         if (!response.ok) {
             throw new Error(response.status);
